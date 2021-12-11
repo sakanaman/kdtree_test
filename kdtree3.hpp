c37dbb2ab8fddd298126b4cbc6630e496f994b26
sakanaman <sakana1231@icloud.com>
2021-12-11 23:03:01 +0900

    wrote knn algorithm but undebugged

--- kdtree3.hpp
+++ kdtree3.hpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <array>
 #include <functional>
+#include <cmath>
+#include <queue>
 
 namespace kdtree
 {
@@ -45,7 +47,13 @@ public:
                      int depth,
                      Points& res);
 
+    void find_knn(const int k, const Point& p, Points& res);
+    
+    void find_knn_imp(const int v, const int k, const Point& p, int depth, 
+                      std::priority_queue<int, std::vector<int>, std::function<bool(int, int)>>& prio_queue);
+
 private:
+
 
     bool is_inside_radius(double x, double y, double z, double px, double py, double pz, double r)
     {
@@ -153,6 +161,84 @@ void Tree::find_radius(const int v,
         }
     }
 }                    
+
+
+double dist(const Point& v1, const Point& v2)
+{
+    double dx = v1[0] - v2[0];
+    double dy = v1[1] - v2[1];
+    double dz = v1[2] - v2[2];
+    return std::sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+
+void Tree::find_knn(const int k, const Point& p, Points& res)
+{
+    auto compare = [&](int a, int b)
+    {
+        Point one = _access(a);
+        Point other = _access(b);
+        return dist(one, p) < dist(other, p);
+    };
+    std::priority_queue<int, std::vector<int>, std::function<bool(int, int)>> prio_queue{compare};
+
+    find_knn_imp(0, k, p, 0, prio_queue);
+
+    res.resize(k);
+    for(int i = 0; i < k; ++i)
+    {
+        res[i] = _access(prio_queue.top());
+        prio_queue.pop();
+    }
+}
+
+void Tree::find_knn_imp(const int v, const int k, const Point& p, int depth, 
+                        std::priority_queue<int, std::vector<int>, std::function<bool(int, int)>>& prio_queue 
+                       )
+{
+    int now_index = _points[_nodes[v].location];
+    Point now_coord = _access(now_index);
+    double now_dist = dist(now_coord, p);
+
+    if(prio_queue.size() < k)
+    {
+        prio_queue.push(now_index);
+        if(_nodes[v].left != NIL)
+        {
+            find_knn_imp(_nodes[v].left, k, p, depth+1, prio_queue);
+        }
+        if(_nodes[v].right != NIL)
+        {
+            find_knn_imp(_nodes[v].right, k, p, depth+1, prio_queue);
+        }
+    }
+    else
+    {
+        int top_index = prio_queue.top();
+        Point top_coord = _access(top_index);
+        double top_dist = dist(p, top_coord);
+        if(now_dist < top_dist)
+        {
+            prio_queue.pop();
+            prio_queue.push(now_index);
+            // renew top_*
+            top_index = prio_queue.top();
+            top_coord = _access(top_index);
+            top_dist = dist(p, top_coord);
+        }
+        int axis = depth % 3;
+        if(_nodes[v].left != NIL && p[axis] - top_dist < now_coord[axis])
+        {
+            find_knn_imp(_nodes[v].left, k, p, depth+1, prio_queue);
+        }
+        if(_nodes[v].right != NIL && now_coord[axis] < p[axis] + top_dist)
+        {
+            find_knn_imp(_nodes[v].right, k, p, depth+1, prio_queue);
+        }
+    }
+}                        
+
+
 
 } // namespace dim2
 
